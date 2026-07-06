770d892776743ff065e7c00e323a51d82c889bf4
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:12:31 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,36 @@ output "dns_a_records" {
   description = "All dns_a_record resources"
   value       = azurerm_dns_a_record.dns_a_records
 }
+output "dns_a_records_fqdn" {
+  description = "List of fqdn values across all dns_a_records"
+  value       = [for k, v in azurerm_dns_a_record.dns_a_records : v.fqdn]
+}
+output "dns_a_records_name" {
+  description = "List of name values across all dns_a_records"
+  value       = [for k, v in azurerm_dns_a_record.dns_a_records : v.name]
+}
+output "dns_a_records_records" {
+  description = "List of records values across all dns_a_records"
+  value       = [for k, v in azurerm_dns_a_record.dns_a_records : v.records]
+}
+output "dns_a_records_resource_group_name" {
+  description = "List of resource_group_name values across all dns_a_records"
+  value       = [for k, v in azurerm_dns_a_record.dns_a_records : v.resource_group_name]
+}
+output "dns_a_records_tags" {
+  description = "List of tags values across all dns_a_records"
+  value       = [for k, v in azurerm_dns_a_record.dns_a_records : v.tags]
+}
+output "dns_a_records_target_resource_id" {
+  description = "List of target_resource_id values across all dns_a_records"
+  value       = [for k, v in azurerm_dns_a_record.dns_a_records : v.target_resource_id]
+}
+output "dns_a_records_ttl" {
+  description = "List of ttl values across all dns_a_records"
+  value       = [for k, v in azurerm_dns_a_record.dns_a_records : v.ttl]
+}
+output "dns_a_records_zone_name" {
+  description = "List of zone_name values across all dns_a_records"
+  value       = [for k, v in azurerm_dns_a_record.dns_a_records : v.zone_name]
+}
 
